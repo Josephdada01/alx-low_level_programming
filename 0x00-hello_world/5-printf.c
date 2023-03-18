31e708aebd306cc7e7664e9b613a8d8745c4002d
Josephdada01 <ridwandada500@gmail.com>
2023-03-17 17:31:26 -0700

    made a slight changes on the commands

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * Description: main - prints out a string to stdout.
- * return: 0 if succes.
- * */
-int main(void) 
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 printf("with proper grammar,but the outcome is a piece of art,\n");
 return (0);
