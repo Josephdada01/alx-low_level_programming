eb6ec7719bbfd132d2b63e546cec67db52cb6710
Josephdada01 <ridwandada500@gmail.com>
2023-03-18 01:30:22 -0700

    created a 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora korpa, 2015-10-19\", stdout);
+write(2, "and that piece of art is useful\" - Dora korpa, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora korpa, 2015-10-19"));
 return (1);
 }
