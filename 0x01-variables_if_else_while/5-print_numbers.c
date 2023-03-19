c32b1b2ea51a0279c332c5eff98bca197e5b8418
Josephdada01 <ridwandada500@gmail.com>
2023-03-19 16:17:52 -0700

    printing out single digit of base ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: printing all the single numbers of base ten
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d\n", x);
+	}
+	return (0);
+}
