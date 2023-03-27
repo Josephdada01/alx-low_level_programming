eb41d9ef6e7be3e645013c330b4ec6a8898d1434
Josephdada01 <ridwandada500@gmail.com>
2023-03-27 14:53:18 -0700

    setting a pointer to a parameter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - a funtion that takes pointer to an int and set value to 98
+ *
+ * @n: parameter thta need to be set to 98
+ *
+ * Return: 0 Success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
