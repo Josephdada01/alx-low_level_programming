ac4289c43883d99937499dbe0b1ade801f5e143a
Josephdada01 <ridwandada500@gmail.com>
2023-03-27 15:39:05 -0700

    I swap to integer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - fuction that swap the value of int a and int b
+ * @a: the parameter
+ * @b: the second parameter
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+
+	int exchange;
+
+	exchange =  *a;
+ 	*a =  *b;
+ 	*b = exchange;
+}
