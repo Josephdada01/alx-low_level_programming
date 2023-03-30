343fc71c5a2dc4dc34615bcd8ed2674c5c36a0da
Josephdada01 <ridwandada500@gmail.com>
2023-03-30 01:16:15 -0700

    comparing two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,9 +15,12 @@ int _strcmp(char *s1, char *s2)
 	s = 0;
 
 	while (s1[s] == s2[s])
-	if (s1[s] == '\0')
 	{
-		return (0);
+		if (s1[s] == '\0')
+		{
+			return (0);
+		}
+		s++;
 	}
 	return (s1[s] - s2[s]);
 }
