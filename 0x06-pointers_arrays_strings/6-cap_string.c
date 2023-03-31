22b44f6ca0f2af5648c1c900e7ee8359c9f934b1
Josephdada01 <ridwandada500@gmail.com>
2023-03-31 05:15:32 -0700

    a function that capitalize string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -26,9 +26,9 @@ char *cap_string(char *str)
 				|| str[i] == '(' || str[i] == ')' || str[i] == '{'
 				|| str[i] == '}')
 
-		if (str[i] >= 97 && (str[i] <= 122))
+		if (str[i + 1] >= 97 && (str[i + 1] <= 122))
 		{
-			(str[i] = str[i] - 32);
+			(str[i + 1] = str[i + 1] - 32);
 		}
 		i++;
 	}
