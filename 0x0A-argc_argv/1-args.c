84257f244dcbb938484b7c63e98d988498578e87
Josephdada01 <ridwandada500@gmail.com>
2023-04-15 08:48:36 -0700

    a program that print arguments it receives

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a function that print its numbers
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: 0 on success
+ */
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	{
+		printf("This is the number of argumements: %d\n", argc);
+	}
+	return (0);
+}
