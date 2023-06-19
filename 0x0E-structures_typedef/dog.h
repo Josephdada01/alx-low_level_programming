e639679f199a5d289986d3bf1927d06ce08612cd
Josephdada01 <ridwandada500@gmail.com>
2023-06-19 15:13:55 -0700

    structure project

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a struct about dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the ownwer of the dog
+ * Description: the structure is about a fuction of a particular dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
