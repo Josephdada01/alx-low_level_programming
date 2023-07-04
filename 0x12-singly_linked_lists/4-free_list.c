b84914197d0b01f70662292837a22dec560833cd
Josephdada01 <ridwandada500@gmail.com>
2023-07-04 08:28:56 -0700

    update

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_list - a function that free struct list
+ * @head: the head of the node
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *next;
+
+	while (!current)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
