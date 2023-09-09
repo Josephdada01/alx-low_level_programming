b86154e5c299946cfb5b123141c5e007958b6ad5
Josephdada01 <ridwandada500@gmail.com>
2023-09-09 13:12:26 -0700

    omo

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,6 +14,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		return (NULL);
 	}
+	if (table == NULL)
+	{
+		return (NULL);
+	}
 	table->size = size;
 	table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,8 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -9,15 +12,11 @@
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
  */
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdio.h>
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -30,8 +29,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
