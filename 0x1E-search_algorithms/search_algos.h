4538c079d73a3d542428c1fda277e5af367af228
Josephdada01 <ridwandada500@gmail.com>
2024-01-20 12:57:07 +0100

    firt advanced task

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,6 +3,9 @@
 
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
